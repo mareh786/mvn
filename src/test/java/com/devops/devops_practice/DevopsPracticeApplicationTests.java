98da3b515137314739a955a232ede0ecc4e8c113
Mohammed adil <mohammedadilrehan@gmail.com>
2024-08-31 17:43:21 +0530

    added files

---
+++ src/test/java/com/devops/devops_practice/DevopsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devops_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
